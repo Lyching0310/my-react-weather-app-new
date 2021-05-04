b3185bb6c8ecf4efdb6e3d363b00bf9f01a1db22
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-05-04 14:49:44 +0100

    added github url

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
+    <div id="footer">
+      Created by Alyssa Reyes {""}
+      <a
+        href="https://github.com/Lyching0310/my-react-weather-app-new"
+        target="_blank"
+        rel="noreferrer"
+        id="gitLink"
+      >
+        Open-source code
+      </a>
+      {""} and {""}
+      <a
+        href="https://reactweather-app-alyssareyes.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+        id="Netlify"
+      >
+        Hosted on Netlify
+      </a>
     </div>
   );
 }

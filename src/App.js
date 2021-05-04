4cb5b8d174eebf3d2106309816d8af2257a801c0
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-05-04 15:00:10 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div id="footer">
-      Created by Alyssa Reyes {""}
-      <a
-        href="https://github.com/Lyching0310/my-react-weather-app-new"
-        target="_blank"
-        rel="noreferrer"
-        id="gitLink"
-      >
-        Open-source code
-      </a>
-      {""} and {""}
-      <a
-        href="https://reactweather-app-alyssareyes.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-        id="Netlify"
-      >
-        Hosted on Netlify
-      </a>
+    <div className="App">
+      <div className="container">
+        <div id="footer">
+          <h1>Welcome</h1>
+          Created by Alyssa Reyes {""}
+          <a
+            href="https://github.com/Lyching0310/my-react-weather-app-new"
+            target="_blank"
+            rel="noreferrer"
+            id="gitLink"
+          >
+            Open-source code
+          </a>
+          {""} and {""}
+          <a
+            href="https://reactweather-app-alyssareyes.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+            id="Netlify"
+          >
+            Hosted on Netlify
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
